edb0981f3b6524f66120f3e6f1a80bc56144bc2e
Austin McNicholas <austinmcnicholas@yahoo.com>
2023-09-10 23:03:09 -0500

    Update CoachDashboard.py
    New Logic for Renewal Dates on Roster Search/ Upcoming Renewals & Past Renewals on Main Menu

--- CoachDashboard.py
+++ CoachDashboard.py
@@ -4,16 +4,14 @@ import altair as alt
 import pyperclip
 from datetime import datetime
 
-
 # Load your Excel data 
 df = pd.read_excel(
-        io='Elite.xlsx',
-        engine='openpyxl',
-        sheet_name='Coach Austin',
-        skiprows=[],
-        nrows=127,
+    io='Elite.xlsx',
+    engine='openpyxl',
+    sheet_name='Coach Austin',
+    skiprows=[],
+    nrows=127,
 )
-
 
 # Define a placeholder for the selected username and profile picture
 selected_username = None
@@ -23,24 +21,46 @@ profile_picture = None
 if 'user_notes' not in st.session_state:
     st.session_state.user_notes = {}
 
-def calculate_renewal_date(start_date, membership_type):
-    if membership_type == 'Legacy':
+def calculate_renewal_date(start_date, membership_type, sub_type, renewal):
+    if  sub_type == "3 Months":
+        # Check if the current renewal date has passed
+        today = pd.Timestamp(datetime.today().date())
+        #Check if the current legacy renewal date has passed
+        if renewal <= today:
+            #Calculate the new renewal date as 3 months from today
+            return(renewal + pd.DateOffset(months=3)).strftime('%m/%d/%Y')
+        else:
+            return renewal.strftime('%m/%d/%y')       
+        
+    elif sub_type == 'Monthly':
+        #Check if the current renewl date has passed
+        today_monthly = pd.Timestamp(datetime.today().date())
+        if renewal <= today_monthly:
+            # Calculate the new renewal date as 1 month from today
+            return (renewal + pd.DateOffset(months=1)).strftime('%m/%d/%Y')
+        else:
+            return renewal.strftime('%m/%d/%Y')
+
+    elif membership_type == 'Legacy':
         # Calculate the renewal date as 3 months from the start date
-        renewal_date = start_date + pd.DateOffset(months=3)
-        return renewal_date.strftime('%m/%d/%Y')  # Format date as mm/dd/yyyy
+        return (start_date + pd.DateOffset(months=3)).strftime('%m/%d/%Y')  # Format date as mm/dd/yyyy
     
+    elif sub_type == 'Annual':
+        # Calculate the renewal date as 12 months from the start date (for Annual sub_type)
+        return (start_date + pd.DateOffset(years=1)).strftime('%m/%d/%Y')  # Format date as mm/dd/yyyy
+
     elif membership_type == "Pro":
         # Calculate the renewal date as 1 month from the start date
-        renewal_date_pro = start_date + pd.DateOffset(months=1)
-        return renewal_date_pro.strftime('%m/%d/%Y') # Format date as mm/dd/yyyy
+        return (start_date + pd.DateOffset(months=1)).strftime('%m/%d/%Y') # Format date as mm/dd/yyyy
 
     elif membership_type == "Elite Academy":
-        # Calculate the renewal date as 1 month from the start date
-        renewal_date_elite = start_date + pd.DateOffset(months=1)
-        return renewal_date_elite.strftime('%m/%d/%Y') # Format date as mm/dd/yyyy
+        # Calculate the renewal date as 1 month from the start date (monthly sub type)
+        return (start_date + pd.DateOffset(months=1)).strftime('%m/%d/%Y') # Format date as mm/dd/yyyy
     
     else:
         return " "
+
+
 
 def format_date(date):
     if pd.notna(date):
@@ -93,11 +113,15 @@ def roster_search(df):
 
         # Display the membership type
         st.write(f"**Membership Type:** {selected_profile['Membership']}")
+        
+        # Display the sub_type
+        st.write(f"**Sub Type:** {selected_profile['Sub_Type']}")
 
         selected_start_date = selected_profile['Start_Date']
+        selected_sub_type = selected_profile['Sub_Type']
         if not pd.isna(selected_start_date):
             st.subheader("Renewal Date:")
-            st.markdown(f"<p style='color:red;text-align:center;font-size:24px;'>🔥 {calculate_renewal_date(selected_start_date, selected_profile['Membership'])} 🔥</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='color:red;text-align:center;font-size:24px;'>🔥 {calculate_renewal_date(selected_start_date, selected_profile['Membership'], selected_sub_type, selected_profile['Renewal'])} 🔥</p>", unsafe_allow_html=True)
 
         # Display the user's start date as text
         if not pd.isna(selected_start_date):
@@ -129,7 +153,7 @@ def main():
     )
 
     if choice == "Main Menu":
-        st.subheader(":baseball: Main Menu")
+        st.subheader(":baseball: Coach Austin Dashboard")
         st.markdown("##")
 
         # Filter DataFrame based on selected memberships
@@ -144,28 +168,43 @@ def main():
         # Calculate revenue (replace 'Your_Column_Name_Here' with the actual column name)
         revenue = df_selection['Payment_Per_Month'].sum()
 
-        left_column, middle_column, right_column, right_column_dante = st.columns(4)
-        with left_column:
-            st.subheader(":100: Roster Count")
+        left_column2, left_column1, middle_column, right_column1, right_column2 = st.columns(5)
+        with left_column2:
+            st.subheader(" Roster Count")
             st.subheader(str(total_members))
-        with middle_column:
-            st.subheader(":sparkling_heart: Coach Rating")
-            st.write(f"{average_rating}", key="average_rating")
-        with right_column:
-            st.subheader(":money_mouth_face: Monthly Revenue")
+
+        with left_column1:
+            st.subheader(" Monthly Revenue")
             st.subheader(revenue)
-        # Display the 5 most recent upcoming usernames with renewal dates in the right column
-        with right_column_dante:
-            st.subheader(":pencil: Renewals To Come")
+       
+        # Display the 3 most recent upcoming usernames with renewal dates in the right column
+        with middle_column:
+            st.subheader("New Renewals")
             today = pd.Timestamp(datetime.today().date())  # Convert to Pandas Timestamp
-            future_profiles = df_selection[df_selection['Renewal'] > today].sort_values(by='Renewal').head(5)
-            
+            future_profiles = df_selection[df_selection['Renewal'] > today].sort_values(by='Renewal').head(3)
+
             if not future_profiles.empty:
                 for index, row in future_profiles.iterrows():
                     st.write(f"{row['Username']} - {format_date(row['Renewal'])}")
             else:
                 st.write("No upcoming renewals.")
-            
+
+        # Display the most recent renewals that have already passed in right_column2
+        with right_column1:
+            st.subheader("Past Renewals")
+            today_renewals = pd.Timestamp(datetime.today().date())
+            past_renewals = df_selection[df_selection['Renewal'] < today_renewals].sort_values(by='Renewal', ascending=False).head(3)  # Sort by Renewal in descending order to get the most recent
+
+            if not past_renewals.empty:
+                for index, row in past_renewals.iterrows():
+                    st.write(f"{row['Username']} - {format_date(row['Renewal'])}")
+            else:
+                st.write("No recent renewals.")
+
+        with right_column2:
+            st.subheader(" Coach Rating")
+            st.write(f"{average_rating}", key="average_rating")
+
 
         st.markdown("---")
 
@@ -256,7 +295,6 @@ Former Professional Baseball Player, University of Texas
             pyperclip.copy(text)
             st.success("Copied Text")
 
-            
     else:
         st.subheader("About")
 
